e80bfa986c0535c2d62eb350ceadc2d9f37bc688
kkyoni <modijaymin86@gmail.com>
2022-11-29 18:13:40 +0530

    update

--- storage/framework/views/6393e21c628b533ec08f8d9468c4f16ed3861835.php
+++
@@ -1,2 +0,0 @@
-(function(window,$){window.LaravelDataTables=window.LaravelDataTables||{};window.LaravelDataTables["%1$s"]=$("#%1$s").DataTable(%2$s);})(window,jQuery);
-<?php /**PATH /var/www/html/particle_role/resources/views/vendor/datatables/script.blade.php ENDPATH**/ ?>
--- storage/framework/views/c223a55f5020a795aab8e76699e3465fb75dab27.php
+++
@@ -1,6 +0,0 @@
-<script src="<?php echo e(asset('assets/admin/js/jquery-3.1.1.min.js')); ?>"></script>
-<script src="<?php echo e(asset('assets/admin/js/popper.min.js')); ?>"></script>
-<script src="<?php echo e(asset('assets/admin/js/bootstrap.js')); ?>"></script>
-<?php echo Notify::render(); ?>
-
-<?php echo $__env->yieldContent('authScripts'); ?><?php /**PATH /var/www/html/particle_role/resources/views/admin/includes/scriptsAuth.blade.php ENDPATH**/ ?>
